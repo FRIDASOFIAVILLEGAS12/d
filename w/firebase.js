c40e8020468b8fb06fa287f6fd1fd32cc7cedf6c
HKDHDHK <vigf051108mmclrra4soycecytem.mx>
2022-11-30 09:55:57 -0600

    UYFEUIDGFDE{

--- w/firebase.js
+++ w/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +17,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig); 
+export const app = initializeApp(firebaseConfig);
